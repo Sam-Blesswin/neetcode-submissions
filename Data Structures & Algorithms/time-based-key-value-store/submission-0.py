907b879602d325efdc7096a9926f1849b9be5990
Sam Blesswin <43824496+Sam-Blesswin@users.noreply.github.com>
2026-09-12 15:55:33 -0400

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = {}
+        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashmap:
+            self.hashmap[key] = []
+        
+        self.hashmap[key].append([timestamp,value])
+        
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        
+        values = self.hashmap[key]
+        l,r = 0, len(values)-1
+
+        res = ""
+        while l<=r:
+            mid = (l+r)//2
+            if values[mid][0] == timestamp:
+                res = values[mid][1]
+                break
+            elif values[mid][0] < timestamp:
+                res = values[mid][1]
+                l = mid+1
+            else:
+                r = mid-1
+        return res
+
+
+        
